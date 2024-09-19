eb3c5fd127f844bc4e3ae60f4c608cb877c46df1
Eric Arnou Santos <eric.arnou@tecnico.ulisboa.pt>
2024-09-18 21:07:09 -0300

    feat: implement controller and service to get a list of patients also, implement the pagination and ordering. closes #31 closes #32 closes #33

--- Voll-API/src/main/java/med/Voll/API/controller/PatientController.java
+++ Voll-API/src/main/java/med/Voll/API/controller/PatientController.java
@@ -2,12 +2,12 @@ package med.Voll.API.controller;
 
 import jakarta.validation.Valid;
 import med.Voll.API.model.patient.RegisterPatientDto;
+import med.Voll.API.model.patient.ReturnPatientDto;
 import med.Voll.API.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -19,5 +19,10 @@ public class PatientController {
     @PostMapping()
     public void registerPatient(@RequestBody @Valid RegisterPatientDto registerPatientDto){
         patientService.registerPatient(registerPatientDto);
+    }
+
+    @GetMapping()
+    public Page<ReturnPatientDto> getListOfPatients(Pageable pageable){
+        return patientService.getListOfPatients(pageable);
     }
 }
--- Voll-API/src/main/java/med/Voll/API/service/PatientService.java
+++ Voll-API/src/main/java/med/Voll/API/service/PatientService.java
@@ -3,8 +3,12 @@ package med.Voll.API.service;
 import med.Voll.API.model.patient.Patient;
 import med.Voll.API.model.patient.PatientRepository;
 import med.Voll.API.model.patient.RegisterPatientDto;
+import med.Voll.API.model.patient.ReturnPatientDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -16,5 +20,10 @@ public class PatientService {
     @Transactional
     public void registerPatient(RegisterPatientDto registerPatientDto) {
         patientRepository.save(new Patient(registerPatientDto));
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public Page<ReturnPatientDto> getListOfPatients(Pageable pageable) {
+        return patientRepository.findAll(pageable).map(ReturnPatientDto::new);
     }
 }
